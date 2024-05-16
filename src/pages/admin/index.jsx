3414c56ad82860c6697cfe022609c3f50a4e2039
JoshuaMadd <joshua.maddelein@student.vives.be>
2024-05-16 15:55:19 +0200

    noAlert

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,8 +1,21 @@
 import BaseLayout from "@/layout/BaseLayout";
 import LijstSeizoen from "../../components/admin/LijstSeizoen";
 import Users from "@/components/admin/users/users";
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+ 
+ 
 export default function Home() {
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const isAdmin = localStorage.getItem('admin');
+    console.log(isAdmin)
+    if (isAdmin === 'false') {
+      router.push('/');
+    }
+  }, []);
+ 
   return (
     <BaseLayout>
       <div className="header">
